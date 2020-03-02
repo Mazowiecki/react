057dd72c0316d40a58085ace6cd9a9c8e28d9b70
mazowies <stanislaw.mazowiecki@contractors.roche.com>
2020-03-02 12:08:23 +0100

    Initial commit

---
+++ src/components/testC/testC.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './testC.module.scss';
+
+const testC = () => (
+  <div className={styles.testC}>
+    testC Component
+  </div>
+);
+
+testC.propTypes = {};
+
+testC.defaultProps = {};
+
+export default testC;
